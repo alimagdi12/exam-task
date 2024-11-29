1f8129abc21453d36d0ca656579b5b5f34fac15c
alimagdi12 <alimagdi12367@yahoo.com>
2024-11-29 18:52:18 +0200

    adding vite.config file

--- Backend/mongoDB_connection/mongoose.connect.js
+++ Backend/mongoDB_connection/mongoose.connect.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 exports.connect = () => {
     return mongoose.connect(
-        `${process.env.MONGODBURL}/quiz-task?retryWrites=true&w=majority`,
+        `mongodb+srv://admin:XHHU9hPLA5Sw7K6J@cluster0.jynhvzr.mongodb.net/quiz-task?retryWrites=true&w=majority`,
     );
 };
